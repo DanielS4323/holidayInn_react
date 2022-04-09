91465ae5c2a9bcc47d5da3ec7e76609f9861d1f7
DanielS4323 <daky4323@gmail.com>
2022-04-09 15:36:45 +0200

    finished

--- src/components/addNew/AddNew.js
+++ src/components/addNew/AddNew.js
@@ -1,16 +1,62 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import defImg from '../../assets/images copy/hotel-default.jpg'
+import { getAllAccoms, postAccom, postImage } from '../../service/service'
 
 function AddNew() {
 
   const [newImage, SetNewImage] = useState(null)
+  const [name, SetName] = useState('')
+  const [city, SetCity] = useState('')
+  const [address, SetAddress] = useState('')
+  const [rating, SetRating] = useState(0)
+  const [description, SetDescription] = useState('')
+  const [image, SetImage] = useState('')
+  const [allAccoms, SetAllAccoms] = useState([])
+
+  useEffect(() => {
+      getAllAccoms().then((res => {
+        SetAllAccoms(res.data)        
+      })
+  )},[])
+
+  const postImg = () => {
+    const formData = new FormData()
+      if(newImage) {
+        formData.append('image',newImage)
+      }       
+      postImage(formData).then((res) => {
+       SetImage(res.data.filename)
+      })
+  }
+
+
+  const postHotel = () => {
+      if(!name || !city || !address || !rating || !description || !image) {
+        console.log('nije proso');
+        return 
+      }
+      let obj = {
+        _id: (allAccoms.length * 2) + 1,
+        description,
+        rating,
+        address,
+        name,
+        city,
+        image 
+      }
+      
+      postAccom(obj).then((res) => {
+        console.log('Sent');
+      })
+     
+  }
 
   return (
    <div className='row'>
      <div className='col-6'>
        <input type='file' onChange={(e)=>SetNewImage(e.target.files[0])}/>
        <div className="col-6 text-end">
-         <button className='btn btn-secondary my-3'>Upload</button>
+         <button className='btn btn-secondary my-3' onClick={postImg}>Upload</button>
          <div className='col my-3'>
          <img src={newImage ? URL.createObjectURL(newImage) : defImg} alt='default' className='img-fluid'/>
 
@@ -19,35 +65,35 @@ function AddNew() {
      </div>
     <div className='col-6'>
      
-      <form>
+    
             <div className="form-group">
               <label htmlFor="name">Name</label>
-              <input type="text" className="form-control" name="name" id="name" placeholder="Name" 
+              <input onChange={(e) => SetName(e.target.value)} type="text" className="form-control" name="name" id="name" placeholder="Name" 
               />
            
             </div>
             <div className="form-group">
               <label  htmlFor="place">Place</label>
-              <input type="text" className="form-control" name="place" id="place" placeholder=""
+              <input onChange={(e) => SetCity(e.target.value)} type="text" className="form-control" name="place" id="place" placeholder=""
               />
             </div>
             <div className="form-group">
               <label htmlFor="address">Address</label>
-              <input type="text" className="form-control" name="address" id="address" placeholder="Address"
+              <input onChange={(e) => SetAddress(e.target.value)} type="text" className="form-control" name="address" id="address" placeholder="Address"
               />
             </div>
             <div className="form-group">
               <label htmlFor="rating">Rating</label>
-              <input type="number" className="form-control" name="rating" id="rating" placeholder="0"
+              <input onChange={(e) => SetRating(e.target.value)} type="number" className="form-control" name="rating" id="rating" placeholder="0"
               />
             </div>
             <div className="form-group">
                 <label htmlFor="desc">Description</label>
-                <textarea className="form-control" name="desc" id="desc" rows="3"
+                <textarea onChange={(e) => SetDescription(e.target.value)} className="form-control" name="desc" id="desc" rows="3"
                 ></textarea>
               </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
-          </form>
+            <button type="submit" className="btn btn-primary" onClick={postHotel}>Submit</button>
+          
     
     </div>
 
--- src/service/service.js
+++ src/service/service.js
@@ -14,4 +14,5 @@ export const getAllAccoms = (select, pgSize, ratingFrom, ratingTo) => axios.get(
 export const getDetails = (id) => axios.get(`http://localhost:3000/api/accommodations/${id}`)
 export const getReviews = (id) => axios.get(`http://localhost:3000/api/accommodations/${id}/reviews`)
 export const getPlaces = () => axios.get('http://localhost:3000/api/places')
-export const postAccom = (accomm) => axios.post('http://localhost:3000/api/accommodations', {accomm})
+export const postAccom = (accomm) => axios.post('http://localhost:3000/api/accommodations', accomm)
+export const postImage = (formData) => axios.post('http://localhost:3000/image', formData)
